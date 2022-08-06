dce0e16d2d35cb002c17f14d9435dd2e6d942949
Zac <60305207+zsinclair1506@users.noreply.github.com>
2022-08-07 00:07:52 +1000

    Commit 6.1 - Updated ToDo/Roadmap
    as above

--- src/main/java/net/lynxzz/testmod/TestMod.java
+++ src/main/java/net/lynxzz/testmod/TestMod.java
@@ -14,13 +14,26 @@ public class TestMod implements ModInitializer {
 	/*
 	* TODO:
 	* Lean Recipe
-	* Recipe for Technetium Tools
-	* Effects for technetium tools
 	* Alloying
 	* Chromium Alloy Texture
 	* Chromium Alloy Tools
+	* Chromium Doors
+	* Scrap Alloying for now?
+	* Ore Generation
+	* Custom Bow
+	* Custom flower and crop
 	*
-	*
+	* Post Beta-1 Release
+	* Custom Tree
+	* Custom Music Disc (Lowkey wanna write a song for this)
+	* Custom potion effects and brewing (lean recipe here)
+	* Custom Advancements
+	* Custom Block Entity and Recipe Types (Alloying here)
+	* Custom Fluids (Lean Fluid)
+	* New Ore for End Dimension
+	* 3D endgame armor for End Ore
+	* 
+	* Release 1.0.0 :D
 	 */
 
 	@Override
